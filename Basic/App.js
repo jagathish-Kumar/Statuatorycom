092d07b088462c44bbde36e9c8838ea24ce4c63c
jagathish-Kumar <59340861+jagathish-Kumar@users.noreply.github.com>
2021-08-30 07:49:56 +0530

    Update App.js

--- Basic/App.js
+++ Basic/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App4">
       <h3>Configure PF, ESI, and Professional Tax </h3>
       <div id ="para1">
       <h6 id="parasize">These benefits and taxes are recommended by the government for your employees. Enable the components that are applicable</h6>
